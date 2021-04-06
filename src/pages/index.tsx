985d74980f3ae7742d924aab21e807ad7b228a27
kiyo <kiyonari6318@yahoo.co.jp>
2021-04-06 17:40:53 +0900

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Box, Heading } from "@chakra-ui/react"
+import React from "react"
+
+export default function HomePage() {
+  return (
+    <Box>
+      <Heading>Hello World!</Heading>
+    </Box>
+  )
+}
